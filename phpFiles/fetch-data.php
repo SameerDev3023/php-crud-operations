89b88fb58dc8ca03672743983183048c06deed7f
sameer <sameerdeveloper05@gmail.com>
2023-04-19 22:59:33 +0530

    fetching data from data base through php

--- phpFiles/fetch-data.php
+++ phpFiles/fetch-data.php
@@ -1,3 +1,48 @@
 <?php
-echo'hello world';
+include 'connection.php';
+
+$sql = "SELECT * FROM `crud-operations`";
+$query = mysqli_query($mysqli,$sql);
+echo'
+<table border="1px" cellpadding="15px" cellspacing="0px">
+<thead>
+<tr>
+<th>Id</th>
+<th>Name</th>
+<th>User Name</th>
+<th>Email</th>
+<th>Number</th>
+<th>Password</th>
+<th>Confirm Password</th>
+<th>Gender</th>
+</tr>
+</thead>
+
+';
+if(mysqli_num_rows($query)>0){
+    while($row = mysqli_fetch_assoc($query)){
+        echo'
+        <tbody>
+        <tr>
+        <td>'.$row['id'].'</td>
+        <td>'.$row['full-name'].'</td>
+        <td>'.$row['user-name'].'</td>
+        <td>'.$row['email'].'</td>
+        <td>'.$row['number'].'</td>
+        <td>'.$row['pass'].'</td>
+        <td>'.$row['cpass'].'</td>
+        <td>'.$row['gender'].'</td>
+        </tr>
+
+
+        </tbody>
+        
+        
+        ';
+    }
+}else{
+    echo'data not availble'; 
+}
+
+
 ?>
--- phpFiles/insert.php
+++ phpFiles/insert.php
@@ -5,8 +5,12 @@ if(isset($_POST['register'])){
     $username = $_POST['user-name'];
     $email = $_POST['email'];
     $number = $_POST['number'];
-    $pass = $_POST['pass'];
-    $cpass = $_POST['cpass'];
+
+    // crypt function use for making has password like this $1$U0cP3ODq$NXFHbwIeGLeL11v1nh/ad1
+
+    
+    $pass = crypt($_POST['pass']);
+    $cpass = crypt($_POST['cpass']);
     $gender = $_POST['gender']; 
     $sql = "INSERT INTO `crud-operations`(`full-name`, `user-name`, `email`, `number`, `pass`, `cpass`, `gender`) 
             VALUES ('$name',
