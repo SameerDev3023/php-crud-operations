4389fdbe8016d7354dafffe0ee789c5282822008
sameer <sameerdeveloper05@gmail.com>
2023-04-24 11:28:16 +0530

    add comments in code

--- phpFiles/delete.php
+++ phpFiles/delete.php
@@ -4,6 +4,8 @@ include 'connection.php';
     $id = $_GET['id'];
 $query = "DELETE FROM `crud-operations` WHERE `id`=$id";
 if(mysqli_query($mysqli,$query)){
+// redirect to records page when the data is deleted and showing deleted data with header location function ;
+// header is a php function it help us to redirect our pages to another pages  as we want . 
     header('location:fetch-data.php');
 }else{
     echo'data not deleted';
